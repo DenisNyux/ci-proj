098c3c3b30b1ddb2e3c0f3384101978c261b8435
DenisNyux <denisn2000@gmail.com>
2021-10-15 02:47:40 +0300

    Update tests.py

--- counting/tests.py
+++ counting/tests.py
@@ -5,5 +5,5 @@ i = IntegralMethods()
 assert type(i.parabola(-10, 10)) == float
 assert type(i.trapeze(-10, 10)) == float
 assert round(i.parabola(-10, 10), 5) == 3662.928
-assert round(i.trapeze(-10, 10), 5) == 3900.001 #2
+assert round(i.trapeze(-10, 10), 5) == 3900.002 #2
 
